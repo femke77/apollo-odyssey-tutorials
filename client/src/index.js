4cd233c6913067213a1792cc9998d1d5581b68d9
femke77 <megan.meyers.388@gmail.com>
2023-08-24 00:12:01 -0700

    finished lift-off-1

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// Apollo Client is the class that represents apollo on the front end
+const client = new ApolloClient({
+  // link to the server
+  uri: "http://localhost:4000",
+  // use the built in cache
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  // apollo provider provides apollo to react
+  <ApolloProvider client={client}>
     <GlobalStyles />
+
     <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
